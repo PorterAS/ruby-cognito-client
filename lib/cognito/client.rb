36ecbccd09c2965fe8a6ca2d16209fd7562e0dc2
Ilya Bylich <ibylich@gmail.com>
2018-01-08 13:28:15 +0300

    Initial commit.

---
+++ lib/cognito/client.rb
@@ -0,0 +1,7 @@
+require "cognito/client/version"
+
+module Cognito
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ spec/cognito/client_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Cognito::Client do
+  it "has a version number" do
+    expect(Cognito::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
